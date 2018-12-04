8ba25603761018398eec790876be62c9457ceae5
filippawinski <filip@pawinski.info>
2018-12-04 15:57:10 +0100

    Update services.sh

--- install/services/services.sh
+++ install/services/services.sh
@@ -5,20 +5,20 @@
 /etc/init.d/php7.0-fpm restart
 /etc/init.d/lighttpd restart
 
-update-rc.d smstools enable
+#update-rc.d smstools enable
 
-systemctl enable mosquitto
-cp $dir/install/services/nettempmqtt.service /etc/systemd/system/
-systemctl enable nettempmqtt
-systemctl start nettempmqtt
+#systemctl enable mosquitto
+#cp $dir/install/services/nettempmqtt.service /etc/systemd/system/
+#systemctl enable nettempmqtt
+#systemctl start nettempmqtt
 
-if [[ $APCUPS == 'yes' ]]; then
-    /etc/init.d/apcupsd start
-    update-rc.d apcupsd enable
-else
-    /etc/init.d/apcupsd stop
-    update-rc.d apcupsd disable
-fi
+#if [[ $APCUPS == 'yes' ]]; then
+#    /etc/init.d/apcupsd start
+#    update-rc.d apcupsd enable
+#else
+#    /etc/init.d/apcupsd stop
+#    update-rc.d apcupsd disable
+#fi
 } >> $dir/install_log.txt 2>&1
 
 exitstatus=$?

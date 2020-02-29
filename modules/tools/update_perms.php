36cf12c4ad405ba92b2832a61faf54993dabbaa9
filippawinski <filip@pawinski.info>
2020-02-29 17:03:55 +0100

    Update update_perms.php

--- modules/tools/update_perms.php
+++ modules/tools/update_perms.php
@@ -7,8 +7,8 @@ shell_exec("sudo chmod g+s -R $ROOT/tmp");
 shell_exec("sudo setfacl -m d:m::rwx $ROOT/tmp");
 shell_exec("sudo chmod g+s -R $ROOT/db");
 shell_exec("sudo chmod g+s -R $ROOT/dbf");
-shell_exec("sudo chmod a+w -R /var/spool/sms/outgoing");
-shell_exec("sudo chmod -R 644 /var/log/smstools/smsd.log");
+//shell_exec("sudo chmod a+w -R /var/spool/sms/outgoing");
+//shell_exec("sudo chmod -R 644 /var/log/smstools/smsd.log");
 shell_exec("sudo gpasswd -a www-data dialout 1>/dev/null");
 
 ?>
